9668894ad8ad2b7b9333ee6ba3a327d1259d5c90
S4RPYK3IVl5 <S4RPYK3IVl5@users.noreply.github.com>
2019-08-08 15:26:06 +0400

    Backend init

---
+++ Backend/Datebook/src/main/java/dailybook/hello/controllers/MainController.java
@@ -0,0 +1,16 @@
+package dailybook.hello.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String getHello(){
+        return "index";
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Datebook
 Datebook
+
+Google sheet - https://docs.google.com/spreadsheets/d/1V4k1WuBvvMfGZpooItVlFl47K9OmmV3TqHF4T3FKxZI/edit#gid=1386834576
