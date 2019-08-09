459a30de53755aaabefe39a8b1c5bd9476650f87
S4RPYK3IVl5 <S4RPYK3IVl5@users.noreply.github.com>
2019-08-09 18:04:27 +0400

    Add full registration

---
+++ Backend/Datebook/src/main/java/dailybook/hello/domain/User.java
@@ -0,0 +1,61 @@
+package dailybook.hello.domain;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+import java.util.Set;
+
+@Data
+@Table(name = "usr")
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(min = 6, max = 30)
+    private String username;
+
+    @NotNull
+    @Size(min = 6, max = 30)
+    private String password;
+
+    private boolean active;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive();
+    }
+}
---
+++ Backend/Datebook/src/main/java/dailybook/hello/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package dailybook.hello.repos;
+
+import dailybook.hello.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
